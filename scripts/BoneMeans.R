6d60ad77e2675b6bc7cab0d1825fb632f9ea1cc4
jprofijt <joukepro@gmail.com>
2018-09-19 11:22:32 +0200

    Fixed issue with taking means from all measuerements and not separated by length and diameter

--- scripts/BoneMeans.R
+++ scripts/BoneMeans.R
@@ -1,16 +1,22 @@
 # Copyright (c) 2018 Jouke Profijt
 # Licensed under GPLv3, see LICENSE
 
-# Will calculate means for all bones separated by their ecological groups.
+# Will calculate means for all bone lenghts and diameters separated by their ecological groups.
 
-means <- c()
+
 BoneMeans <- function(data) {
+  length.means <- c()
+  diameter.means <- c()
   for(t in data$id){
-    value <- mean(as.numeric(data[data$id == t, 2:11]))
-    means <- append(means, value)
-    
+    length <- c(2,4,6,8,10)
+    diameter <- c(3,5,7,9,11)
+    length.value <- mean(as.numeric(data[data$id == t, length]))
+    length.means <- append(length.means, length.value)
+    diameter.value <- mean(as.numeric(data[data$id == t, diameter]))
+    diameter.means <- append(diameter.means, diameter.value)
   
   }
-  data$means <- means
+  data$length.mean <- length.means
+  data$diameter.mean <- diameter.means
   return(data)
 }
