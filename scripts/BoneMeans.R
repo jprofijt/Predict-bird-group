6e0dfee2cbcba408dc9883e1d4978767d63f166d
jprofijt <joukepro@gmail.com>
2018-09-27 09:51:42 +0200

    Added dynamic collum selecting to filter out bones which are less important for classification

--- scripts/BoneMeans.R
+++ scripts/BoneMeans.R
@@ -4,12 +4,10 @@
 # Will calculate means for all bone lenghts and diameters separated by their ecological groups.
 
 
-BoneMeans <- function(data) {
+BoneMeans <- function(data, length, diameter) {
   length.means <- c()
   diameter.means <- c()
   for(t in data$id){
-    length <- c(2,4,6,8,10)
-    diameter <- c(3,5,7,9,11)
     length.value <- mean(as.numeric(data[data$id == t, length]))
     length.means <- append(length.means, length.value)
     diameter.value <- mean(as.numeric(data[data$id == t, diameter]))
